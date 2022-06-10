a1c2737647a39bc357d945f8e13baeb64c038b2c
seokmin <raph03@g.skku.edu>
2022-06-10 14:36:22 +0900

    warp_image 함수 주석

--- preliminary/driving.py
+++ preliminary/driving.py
@@ -61,9 +61,13 @@ def drive(angle, speed):
 
     motor.publish(motor_msg)
 
+#변환이 적용된 이미지(warp_img) 반환
 def warp_image(img, src, dst, size):
+    # 원근 변환을 가져옴
     M = cv2.getPerspectiveTransform(src, dst)
+    # 목표 원근 변환을 가져옴
     Minv = cv2.getPerspectiveTransform(dst, src)
+    # 카메라에서 가져온 이미지를 버드뷰로 전환
     warp_img = cv2.warpPerspective(img, M, size, flags=cv2.INTER_LINEAR)
 
     return warp_img, M, Minv
@@ -71,19 +75,20 @@ def warp_image(img, src, dst, size):
 
 def warp_process_image(img):
     
-    #bounding box계산을 위한 값
+    # bounding box계산을 위한 값
+    # 박스의 개수
     global nwindows
+    # 박스의 가로 길이
     global margin
     global minpix
     global lane_bin_th
 
     #GaussianBLur와 cvtColor를 통해 BGR에서 HLS로 변환
     blur = cv2.GaussianBlur(img,(5, 5), 0)
-    _, L, _ = cv2.split(cv2.cvtColor(blur, cv2.COLOR_BGR2HLS))
+    _, L, _ = cv2.split(cv2.cvtColor(blur, cv2.COLOR_BGR2HLS)   )
     #threshold를 통해 L채널을 이진화해주며 lane에 넣음.
-    _, lane = cv2.threshold(L, lane_bin_th, 255, cv2. THRESH_BINARY)
-
-    #lane에서 세로축 반으로 slice해준 후에 axis=0 즉 (240,320)인 lane에서 첫번쨰 축을 더해줌으로서
+    _, lane = cv2.threshold(L, lane_bin_th, 255, cv2.       THRESH_BINARY)
+    #lane에서 세로축 반으로 slice해준 후에 axis=0 즉 (240,320)인 lane에서 첫번쨰 축을 더해줌으로
     #(320,)으로 histogram을 만들어 줄 수 있다.
     histogram = np.sum(lane[lane.shape[0]//2:,:],   axis=0)
     #가로 중간지점     
@@ -94,17 +99,17 @@ def warp_process_image(img):
 
     # 차량 주행 중에 차선인식이 일어나지 않는 경우,
     # 해당 차선을 인식할 떄 사용되던 window들의 default위치를 조정.
-    if leftx_current == 0:
+    if (leftx_current == 0):
         leftx_current = int(midpoint - 355 / 2)
                       
-    if rightx_current == midpoint:
+    if rightx_current==midpoint:
         rightx_current = int(midpoint + 355 / 2)
 
-    # 쌓을 window의 height 설정
+    #쌓을 window의 height 설정
     window_height = np.int(lane.shape[0]/nwindows)
     
-    # 240*320 픽셀에 담긴 값중 0이 아닌 index들을 
-    # nz[0]에는 index[row][col] 중에 row파트만 담겨있고 nz[1]에는 col이 담겨있다.
+    #240*320 픽셀에 담긴 값중 0이 아닌 index들을 
+    #nz[0]에는 index[row][col] 중에 row파트만 담겨있고 nz[1]에는 col이 담겨있다.
     nz = lane.nonzero()
 
     left_lane_inds = []
@@ -118,7 +123,8 @@ def warp_process_image(img):
 
     for window in range(nwindows):
         
-        # bounding box 크기 설정
+        
+        #bounding box 크기 설정
         win_yl = lane.shape[0] - (window+1)*window_height
         win_yh = lane.shape[0] - window*window_height
 
@@ -127,27 +133,26 @@ def warp_process_image(img):
         win_xrl = rightx_current - margin
         win_xrh = rightx_current + margin
 
-        # out image에 bounding box 시각화
+        #out image에 bounding box 시각화
         cv2.rectangle(out_img,(win_xll,win_yl),(win_xlh,    win_yh),    (0,255,0), 2) 
         cv2.rectangle(out_img,(win_xrl,win_yl),(win_xrh,    win_yh),    (0,255,0), 2) 
 
-        # 흰점의 픽셀들 중에 window안에 들어오는 픽셀인지 여부를 판단하여 
-        # good_left_inds와 good_right_inds에 담는다.
+        #흰점의 픽셀들 중에 window안에 들어오는 픽셀인지 여부를 판단하여 
+        #good_left_inds와 good_right_inds에 담는다.
         good_left_inds = ((nz[0] >= win_yl)&(nz[0] < win_yh)&   (nz    [1] >= win_xll)&(nz[1] < win_xlh)).nonzero()    [0]
         good_right_inds = ((nz[0] >= win_yl)&(nz[0] < win_yh)   &(nz   [1] >= win_xrl)&(nz[1] < win_xrh)).nonzero()    [0]
 
         left_lane_inds.append(good_left_inds)
         right_lane_inds.append(good_right_inds)
 
-        # nz[1]값들 중에 good_left_inds를 index로 삼는 nz[1]들의 평균을 구해서 leftx_current를 갱신한다.
+        #nz[1]값들 중에 good_left_inds를 index로 삼는 nz[1]들의 평균을 구해서 leftx_current를 갱신한다.
+        #nz[1]값들 중에 good_right_inds를 index로 삼는 nz[1]들의 평균을 구해서 rightx_current를 갱신한다.
         if len(good_left_inds) > minpix:
             leftx_current = np.int(np.mean(nz[1]    [good_left_inds])   )
-
-        # nz[1]값들 중에 good_right_inds를 index로 삼는 nz[1]들의 평균을 구해서 rightx_current를 갱신한다.
         if len(good_right_inds) > minpix:        
             rightx_current = np.int(np.mean(nz[1]       [good_right_inds]))
 
-        # lx ly rx ry에 x,y좌표들의 중심점들을 담아둔다.
+        #lx ly rx ry에 x,y좌표들의 중심점들을 담아둔다.
         lx.append(leftx_current)
         ly.append((win_yl + win_yh)/2)
 
@@ -167,40 +172,24 @@ def warp_process_image(img):
 
     return lfit, rfit
 
-#=============================================
-# lane_tracking 함수
-# 차선인식 알고리즘으로 구한 좌측, 우측 차선의 각도와
-# 도로 중앙에서 떨어진 상대적인 거리를 이용
-# 적절한 gain 값으로 조향각을 계산하여 전달
-#=============================================
-
 def lane_tracking(left_line_angle, right_line_angle, curr_position):
-    '''차선추종 알고리즘'''
-
-    # 경로각은 좌측 차선과 우측 차선의 각도로 계산
+    # Lane tracking algorithm here
     lane_angle = -(0.5*left_line_angle + 0.5*right_line_angle)
 
     k1 = 0.7  # P gain
     k2 = 400   # Distance parameter
 
-    # 중앙에서 조금 벗어났으므로 천천히 조향해도 괜찮은 상황
-    if -0.2 < curr_position < 0.2 : 
+    if -0.2 < curr_position < 0.2 :  # 좀 더 천천히 조향해도 괜찮은 상황
         k2 = 400
-
-    # 중앙에서 많이 벗어났으므로 신속하게 가운데로 들어와야 함
-    elif curr_position > 0.5 or curr_position < -0.5 :  
+    
+    elif curr_position > 0.5 or curr_position < -0.5 :  # 신속하게 가운데로 들어와야 함
         k2 = 250
 
-    # 그 중간의 경우 계수는 linear하게 변화
-    else: 
+    else:   # 그 중간의 경우 계수는 linear 변화
         k2 = -500*abs(curr_position) + 500
 
-    # 도로의 커브 정도와 도로 중앙에서부터의 위치로 조향각 계산
     steer_angle = lane_angle * k1 + math.atan(-180 * (curr_position) / k2) * 180 / math.pi
 
-    # 코너를 돌며 한쪽 차선 인식이 안 되면 조향각과 경로각 중 하나가 음수가 되는 상황이 발생
-    # 이를 해결하기 위해서는 신속하게 도로 중앙으로 들어와 정상적인 차선인식이 되게끔 해야 함.
-    # gain을 부여하여 조향각 수정
     if steer_angle * lane_angle < 0:
         steer_angle = lane_angle * 2.5
 
@@ -302,34 +291,21 @@ def start():
         left_position = ((320 - float(lx[3])) / shift_const) * 2 - 1
         right_position = -((-320 + float(rx[4])) / shift_const) * 2 + 1
         
-        #=================================================
-        # 조향각과 속도를 제어하기 위한 부분.            
-        # 양측 차선 각도와 도로 중앙에 대한 현재 위치로 lane_tracking 함수 실행        
-        # 현재 위치와 커브 상태에 따라 계산된 제어값 전달
-        #=================================================
-
-        # 현재 위치 계산
         position = 0
         
-        # 우측 차선이 인식되지 않으면 좌측 차선을 위주로 현재 위치 계산
         if abs(math.degrees(math.atan(line_right[1]))) < 0.05:
             position = 0.9*left_position + 0.1*right_position
         
-        # 좌측 차선이 인식되지 않으면 우측 차선을 위주로 현재 위치 계산
         elif abs(math.degrees(math.atan(line_left[1]))) < 0.05:
             position = 0.1*left_position + 0.9*right_position
         
-        # 두 차선이 모두 인식되면 두 차선에서 떨어진 거리로 현재 위치 계산
         else:
-            position = (left_position + right_position) / 2
+            position = (left_position+right_position)/2
         
-        # lane_tracking 함수로 조향각 도출
         angle = lane_tracking(left_line_angle, right_line_angle, position)               
         
-        # 기본 속도는 40으로 할당
         speed = 40
         
-        # 급커브라고 생각될 경우 속도를 줄여 안정적인 주행
         if abs(angle) > 7:
             speed = 25
         
@@ -339,7 +315,9 @@ def start():
         cv2.imshow("CAM View", out_img)
         cv2.waitKey(1)
 
+
         drive(angle, speed)
+
 
 #=============================================
 # 메인 함수
