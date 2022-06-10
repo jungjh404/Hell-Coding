27c7372be13f377e24284de30c198d166ce8a9fe
하지훈 <hjh7870@naver.com>
2022-06-10 13:29:01 +0900

    과제2 주석 달았습니다람쥐
    parking.py + comment

--- preliminary/parking.py
+++ preliminary/parking.py
@@ -57,25 +57,31 @@ rospy.Subscriber('ar_pose_marker', AlvarMarkers, callback)
 motor_pub = rospy.Publisher('xycar_motor', xycar_motor, queue_size =1 )
 
 #=========================================
+# 메인 제어를 위한 변수 설정
+#=========================================
+
+# P 제어를 위한 gain 설정
+kp_yaw = 1.8        # yaw gain
+kp_theta = 3        # theta gain
+
+# D 제어를 위한 gain 설정
+kd_yaw = 3          # yaw gain
+kd_theta = 3        # theta gain
+
+# D 제어에서 미분값을 계산하기 위한 변수
+prev_time = 0       # 시간
+prev_yaw = 0        # yaw
+prev_theta = 0      # theta
+
+rate = rospy.Rate(120)
+
+
+#=========================================
 # 메인 루프 
 # 끊임없이 루프를 돌면서 
 # "AR정보 변환처리 +차선위치찾기 +조향각결정 +모터토픽발행" 
 # 작업을 반복적으로 수행함.
 #=========================================
-kp_yaw = 1.8
-kp_theta = 3
-
-ki_yaw = 0
-ki_theta = 0
-
-kd_yaw = 3
-kd_theta = 3
-
-prev_time = 0
-prev_yaw = 0
-prev_theta = 0 
-
-rate = rospy.Rate(120)
 
 while not rospy.is_shutdown():
     # 쿼터니언 형식의 데이터를 오일러 형식의 데이터로 변환
@@ -124,25 +130,41 @@ while not rospy.is_shutdown():
     cv2.imshow('AR Tag Position', img)
     cv2.waitKey(1)
 
-    # 핸들 조향각 angle값 설정하기
-    cur_time = time.time()
+    #=============================================
+    # 조향각과 속도를 제어하기 위한 부분.            
+    # AR 태그까지의 거리 정보와 AR 태그에 대한        
+    # 상대적인 yaw 값을 제어에 활용                  
+    #=============================================
+
+    '''핸들 조향각 anlgle 값 설정하기'''
+
+    # 시간에 대한 변화량을 구하기 위해서는 현재 시간이 필요
+    cur_time = time.time()     
+
+    # DX, DY 값으로 구한 AR 태그까지의 상대적인 각도를 theta라고 함.
     theta = math.degrees(math.atan2(int(arData["DX"]), int(arData["DY"])))
 
-    diff_theta = (theta - prev_theta) / (cur_time - prev_time)
-    diff_yaw = (yaw - prev_yaw) / (cur_time - prev_time)
+    diff_theta = (theta - prev_theta) / (cur_time - prev_time)      # D 제어를 위해 theta의 시간에 대한 변화량 산출
+    diff_yaw = (yaw - prev_yaw) / (cur_time - prev_time)            # D 제어를 위해 yaw의 시간에 대한 변화량 산출
 
+    # P, D 제어를 사용하여 차량의 조향각(angle)을 계산
     angle = kp_yaw * (-yaw) + kd_yaw*diff_yaw + kp_theta * theta + diff_theta * kd_theta
 
+    # 최대 우측 조향각이 50 이므로, 이보다 크면 50으로 고정
     if angle > 50:
         angle = 50
+
+    # 최대 좌측 조향각이 -50 이므로, 이보다 작으면 -50으로 고정
     elif angle < -50:
         angle = -50
 
+    # 디버깅을 위해 theta, yaw, angle, distance 값 표시
     print("theta: "+str(theta)+" yaw: "+str(yaw)+" final angle: "+str(angle)+" distance: "+str(distance))
             
-    # 차량의 속도 speed값 설정하기
+    '''차량의 속도 speed 값 설정하기'''
     speed = 10
     
+    # distance가 가까워질수록 속도를 낮춰 주차함.
     if distance < 100 and distance > 80:
         speed = 5
     elif distance < 80 and distance > 65:
@@ -150,6 +172,9 @@ while not rospy.is_shutdown():
     elif distance < 65:
         speed = 0
     
+    # 카메라 각도에 AR 태그 전체부분이 들어오지 않으면 distance를 인식하지 못하다가
+    # 가까이 다가가면 다시 인식하는 모습을 보임.
+    # 다시 distance가 인식되는 가까운 거리는 주차구역을 벗어난 곳이므로 후진하여 자세 조정
     if distance < 50:
         speed = -2
 
@@ -159,9 +184,10 @@ while not rospy.is_shutdown():
     motor_msg.speed = speed
     motor_pub.publish(motor_msg)
 
-    prev_time = cur_time
-    prev_yaw = yaw
-    prev_theta = theta
+    # D 제어를 위해 이전 값 기억
+    prev_time = cur_time        # 시간에 대한 변화량을 구하기 위해 cur_time의 이전 값 기억
+    prev_yaw = yaw              # 시간에 대한 변화량을 구하기 위해 yaw의 이전 값 기억
+    prev_theta = theta          # 시간에 대한 변화량을 구하기 위해 theta의 이전 값 기억
 
     rate.sleep()    
 
