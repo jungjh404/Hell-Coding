234de7509fcc905a93359ee786ea37b453fbebcc
jungjh404 <jungjh404@gmail.com>
2022-08-01 15:13:28 +0900

    hell coding nav stack v1

---
+++ main_ros/scripts/init_odom.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+import rospy
+from geometry_msgs.msg import Twist
+
+class Init_Odom:
+    def __init__(self):
+        rospy.init_node("init_odom", anonymous=True)
+
+        self.init_odom = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
+        self.rate = rospy.Rate(10)
+
+        self.run()
+
+
+    def run(self):
+        while self.init_odom.get_num_connections() < 1:
+            msg = Twist()
+            
+            msg.linear.x = 0
+            msg.linear.y = 0
+            msg.linear.z = 0
+            msg.angular.x = 0
+            msg.angular.y = 0
+            msg.angular.z = 0
+
+        self.init_odom.publish(msg)
+
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+
+
+if __name__ == "__main__":
+    a = Init_Odom()
