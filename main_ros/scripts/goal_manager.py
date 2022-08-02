d13f309bc748b6df90e4d969b10d8f9e4a96583c
jungjh404 <jungjh404@gmail.com>
2022-08-02 21:51:34 +0900

    goal_manager with multiple goal and init parking

--- main_ros/scripts/goal_manager.py
+++ main_ros/scripts/goal_manager.py
@@ -1,57 +1,87 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
 
 import rospy
 import math
+import actionlib
+import tf
 from tf.transformations import quaternion_from_euler
-# from ar_track_alvar_msgs.msg import AlvarMarker
+from ar_track_alvar_msgs.msg import AlvarMarkers
 from geometry_msgs.msg import PoseStamped
-from move_base_msgs.msg import MoveBaseActionResult
+from move_base_msgs.msg import MoveBaseActionResult, MoveBaseAction
 
 
 class GoalManager:
     def __init__(self):
-        rospy.init_node("Goal_Manager", annoymous=True)
-
+        rospy.init_node("goal_manager", anonymous=True)
         self.goal_cnt = 0
-        self.goal_list = [[1, 0, 0], [7.5, 0, 90]]
+        # self.goal_list = [[5, 0, 0], [10, 0, 90], "parking"]
+        self.goal_list = ["parking"]
+        self.goal_num = len(self.goal_list)
 
-        self.goal_pub = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=1)
-        rospy.Subscriber("/move_base/result", MoveBaseActionResult, self.reach_cb)
+        self.rate = rospy.Rate(10)
 
-        self.goal_pub(self.goal_msg_generate())
+        self.goal_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
+        self.res_sub = rospy.Subscriber("/move_base/result", MoveBaseActionResult, self.reach_cb)
+        self.tf_listener = tf.TransformListener()
+
+        client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+        client.wait_for_server()
+
+        rospy.loginfo("Goal Manager Initialized.")
+
+        self.goal_pub.publish(self.goal_msg_generate())
+        rospy.spin()
 
 
     def goal_msg_generate(self):
-        next_goal_msg = PoseStamped()
-
-        next_goal_msg.header.seq = self.goal_cnt
-        next_goal_msg.header.stamp = rospy.Time.now()
-        next_goal_msg.header.frame_id = "map"
+        goal = PoseStamped()
+        goal.header.frame_id = "map"
+        goal.header.stamp = rospy.Time.now()
 
         target = self.goal_list[self.goal_cnt]
 
-        if target is not None:
-            next_goal_msg.pose.position.x = target[0]
-            next_goal_msg.pose.position.y = target[1]
+        if type(target) == list:
+            goal.pose.position.x = target[0]
+            goal.pose.position.y = target[1]
 
             x, y, z, w = quaternion_from_euler(0, 0, math.radians(target[2]))
-            next_goal_msg.pose.orientation.x = x
-            next_goal_msg.pose.orientation.y = y
-            next_goal_msg.pose.orientation.z = z
-            next_goal_msg.pose.orientation.w = w
+            goal.pose.orientation.x = x
+            goal.pose.orientation.y = y
+            goal.pose.orientation.z = z
+            goal.pose.orientation.w = w
         
-        return next_goal_msg
+        elif target == "parking":
+            alvar_msg = rospy.wait_for_message("/ar_pose_marker", AlvarMarkers, timeout=3)
 
-        
+            if len(alvar_msg.markers) != 1:
+                rospy.loginfo("Two or MoreMarkers or No Marker.")
+                pass
+                
+            else:
+                trans, rot = self.tf_listener.lookupTransform("ar_marker_"+str(alvar_msg.markers[0].id), "map", rospy.Time().now())
+                print(trans, rot)
 
-    def reach_cb(self, msg:MoveBaseActionResult):
+
+
+
+        return goal
+
+
+    def reach_cb(self, msg):
         if msg.status.text == "Goal reached.":
             self.goal_cnt += 1
 
-            next_goal_msg = self.goal_msg_generate()
-
-            self.goal_pub.publish(next_goal_msg)
+            if self.goal_cnt >= self.goal_num:
+                rospy.loginfo("Mission Finished.")
+                exit(0)
+            
+            else:
+                self.goal_pub.publish(self.goal_msg_generate())
 
 
 if __name__ == "__main__":
-    a = GoalManager()
+    try:
+        a = GoalManager()
+    except KeyboardInterrupt:
+        exit(0)
