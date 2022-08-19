54b2cbbccb91ab5e985b59a880e0501815a0c705
jungjh404 <jungjh404@gmail.com>
2022-08-20 00:32:23 +0900

    goal_manager and hell_motor

--- main_ros/scripts/goal_manager.py
+++ main_ros/scripts/goal_manager.py
@@ -5,29 +5,28 @@ import rospy
 import math
 import actionlib
 import tf
-from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from tf.transformations import quaternion_from_euler
 from ar_marker_pose import ar_marker_pose
 
 from ar_track_alvar_msgs.msg import AlvarMarkers
-from geometry_msgs.msg import PoseStamped, Quaternion
-from move_base_msgs.msg import MoveBaseActionResult, MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseActionResult, MoveBaseAction, MoveBaseActionGoal
 # from actionlib_msgs.msg import GoalID
-from xycar_msgs.msg import xycar_motor
 
 
 class GoalManager:
     def __init__(self):
-        rospy.init_node("goal_manager", anonymous=True)
         self.goal_cnt = 0
-        # self.goal_list = [[5, 0, 0], [10, 0, 90]]
-        self.goal_list = [("parking", [0, -0.5, 90])]
+        self.goal_list = [[2, 0, 0], [4, 0, 30]]
+        # self.goal_list = [("p_parking", [0, -0.5, 90])]
         self.goal_num = len(self.goal_list)
 
         self.rate = rospy.Rate(10)
 
         # self.cancel_pub = rospy.Publisher("/move_base/cancel", GoalID, queue_size=1)
-        self.goal_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
+        self.goal_pub = rospy.Publisher("/move_base/goal", MoveBaseActionGoal, queue_size=1)
         self.res_sub = rospy.Subscriber("/move_base/result", MoveBaseActionResult, self.reach_cb)
+        self.stop_line = False
         self.tf_listener = tf.TransformListener()
 
         client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
@@ -40,23 +39,30 @@ class GoalManager:
 
 
     def goal_msg_generate(self):
-        goal = PoseStamped()
+        goal = MoveBaseActionGoal()
         goal.header.frame_id = "map"
         goal.header.stamp = rospy.Time.now()
+        goal.header.seq = self.goal_cnt
+        goal.goal_id.id = str(self.goal_cnt)
 
         target = self.goal_list[self.goal_cnt]
 
+        goal.goal.target_pose.header.frame_id = "map"
+        goal.goal.target_pose.header.stamp.secs = goal.header.stamp.secs
+        goal.goal.target_pose.header.stamp.nsecs = goal.header.stamp.nsecs
+        goal.goal.target_pose.header.seq = self.goal_cnt
+
         if type(target) == list:
-            goal.pose.position.x = target[0]
-            goal.pose.position.y = target[1]
+            goal.goal.target_pose.pose.position.x = target[0]
+            goal.goal.target_pose.pose.position.y = target[1]
 
             x, y, z, w = quaternion_from_euler(0, 0, math.radians(target[2]))
-            goal.pose.orientation.x = x
-            goal.pose.orientation.y = y
-            goal.pose.orientation.z = z
-            goal.pose.orientation.w = w
+            goal.goal.target_pose.pose.orientation.x = x
+            goal.goal.target_pose.pose.orientation.y = y
+            goal.goal.target_pose.pose.orientation.z = z
+            goal.goal.target_pose.pose.orientation.w = w
         
-        elif target[0] == "parking":
+        elif target[0] == "p_parking":
             alvar_msg = rospy.wait_for_message("/ar_pose_marker", AlvarMarkers, timeout=3)
 
             if len(alvar_msg.markers) != 1:
@@ -65,14 +71,14 @@ class GoalManager:
             else:
                 trans = ar_marker_pose(self.tf_listener, alvar_msg.markers[0].id)
                 
-                goal.pose.position.x = trans[0] + target[1][0]
-                goal.pose.position.y = trans[1] + target[1][1]
+                goal.goal.target_pose.pose.position.x = trans[0] + target[1][0]
+                goal.goal.target_pose.pose.position.y = trans[1] + target[1][1]
                 
                 target_rot = quaternion_from_euler(0, 0, math.radians(target[1][2]))
-                goal.pose.orientation.x = target_rot[0]
-                goal.pose.orientation.y = target_rot[1]
-                goal.pose.orientation.z = target_rot[2]
-                goal.pose.orientation.w = target_rot[3]
+                goal.goal.target_pose.pose.orientation.x = target_rot[0]
+                goal.goal.target_pose.pose.orientation.y = target_rot[1]
+                goal.goal.target_pose.pose.orientation.z = target_rot[2]
+                goal.goal.target_pose.pose.orientation.w = target_rot[3]
 
         elif target == "t_parking":
             alvar_msg = rospy.wait_for_message("/ar_pose_marker", AlvarMarkers, timeout=3)
@@ -82,20 +88,16 @@ class GoalManager:
 
             pass
         
-        # stop_line -> goal = current position
         elif target == "stop_line":
             pass
         
-        print(goal)
+        rospy.loginfo("#%d: %f %f %f", self.goal_cnt, target[0], target[1], target[2])
         return goal
-
-    def move_to(self):
-        goal = MoveBaseGoal()
 
 
     def reach_cb(self, msg):
-        # print(msg)
-        if msg.status.text == "Goal reached.":
+        print(msg)
+        if msg.status.text == "Goal reached." and int(msg.status.goal_id.id) == self.goal_cnt:
             self.goal_cnt += 1
 
             if self.goal_cnt >= self.goal_num:
@@ -105,19 +107,13 @@ class GoalManager:
             else:
                 self.goal_pub.publish(self.goal_msg_generate())
 
-        # else:
-        #     self.goal_pub.publish(self.goal_msg_generate())
-
-    def euler_to_quat(self, y, p, r):
-        qx = math.sin(r/2) * math.cos(p/2) * math.cos(y/2) - math.cos(r/2) * math.sin(p/2) * math.sin(y/2)
-        qy = math.cos(r/2) * math.sin(p/2) * math.cos(y/2) + math.sin(r/2) * math.cos(p/2) * math.sin(y/2)
-        qz = math.cos(r/2) * math.cos(p/2) * math.sin(y/2) - math.sin(r/2) * math.sin(p/2) * math.cos(y/2)
-        qw = math.cos(r/2) * math.cos(p/2) * math.cos(y/2) + math.sin(r/2) * math.sin(p/2) * math.sin(y/2)
-        return qx, qy, qz, qw
+        else:
+            self.goal_pub.publish(self.goal_msg_generate())
 
 
 if __name__ == "__main__":
     try:
+        rospy.init_node("goal_manager", anonymous=True)
         a = GoalManager()
     except KeyboardInterrupt:
         exit(0)
---
+++ main_ros/scripts/hell_motor.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+####################################################################
+# 프로그램명 : xycar_motor.py
+# 버 전 : v1.1
+# 본 프로그램은 상업 라이센스에 의해 제공되므로 무단 배포 및 상업적 이용을 금합니다.
+#############################################################################
+
+import rospy, rospkg
+import time
+import serial
+import threading
+import sys
+import os
+import signal
+from geometry_msgs.msg import Twist
+from xycar_msgs.msg import xycar_motor
+from ackermann_msgs.msg import AckermannDriveStamped
+from vesc_msgs.msg import VescStateStamped
+from hell_coding.msg import IsStop
+from goal_manager import GoalManager
+
+
+
+def signal_handler(sig, frame):
+    import time
+    time.sleep(3)
+    os.system('killall -9 python rosout')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+class motor:
+    def __init__(self, goal_manager):
+        deviceSetFunc = [self.set_vesc]
+        self.control = False
+        self.debug = False
+        self.start_time = time.time()
+        self.goal_manager = None
+        self.motor_type = 0
+        deviceSetFunc[self.motor_type]()
+        self.set_parameter()
+        self.ros_init(goal_manager=="true")
+        rospy.spin()
+
+    def set_parameter(self):
+        motor_controller = [
+            [0, 0, 0.0068, 0, 0, 0.08, 0, None, self.auto_drive_vesc]
+        ]
+
+        self.angle_weight = motor_controller[self.motor_type][2]
+        self.angle_bias_1 = motor_controller[self.motor_type][0]
+        self.angle_bias_2 = motor_controller[self.motor_type][1]
+        self.speed_weight = motor_controller[self.motor_type][5]
+        self.speed_bias_1 = motor_controller[self.motor_type][3]
+        self.speed_bias_2 = motor_controller[self.motor_type][4]
+        self.Angle = motor_controller[self.motor_type][6]
+        self.Speed = motor_controller[self.motor_type][6]
+        self.seridev = motor_controller[self.motor_type][7]
+        self.auto_drive = motor_controller[self.motor_type][8]
+        self.angle_offset = 0.0
+        self.last_speed = 0.0
+        self.g_chk_time = 0
+        self.change_vector_term = 0.07
+
+    def set_vesc(self):
+        rospy.Subscriber("/sensors/core", VescStateStamped, self.VescStateCallback, queue_size=1)
+        speed_max = float(rospy.get_param("/vesc_driver/speed_max"))
+        speed_min = float(rospy.get_param("/vesc_driver/speed_min"))
+        speed_to_erpm_gain = float(rospy.get_param("/speed_to_erpm_gain"))
+        self.vesc_smax = speed_max / speed_to_erpm_gain
+        self.vesc_smin = speed_min / speed_to_erpm_gain
+        self.FaultCode = [
+            "FAULT_CODE_NONE",
+            "FAULT_CODE_OVER_VOLTAGE",
+            "FAULT_CODE_UNDER_VOLTAGE",
+            "FAULT_CODE_DRV8302",
+            "FAULT_CODE_ABS_OVER_CURRENT",
+            "FAULT_CODE_OVER_TEMP_FET",
+            "FAULT_CODE_OVER_TEMP_MOTOR"
+        ]
+
+    def VescStateCallback(self, data):
+        self.Battery = data.state.voltage_input
+        faultcode = data.state.fault_code
+        if faultcode:
+            rospy.logfatal(self.FaultCode[faultcode])
+        if ((time.time()-self.start_time) > 1):
+            if self.debug:
+                print(self.Battery)
+            self.start_time = time.time()
+
+    def ros_init(self, goal_manager):
+        rospy.init_node('xycar_motor')
+
+        if self.motor_type == 0:
+            self.ack_msg = AckermannDriveStamped()
+            self.ack_msg.header.frame_id = ''
+            self.ackerm_publisher = rospy.Publisher('ackermann_cmd', AckermannDriveStamped, queue_size=1)
+
+        rospy.Subscriber("cmd_vel", Twist, self.teb_ackermann_callback, queue_size=1) # when using teb local planner
+
+        if goal_manager:
+            self.goal_manager = GoalManager()
+
+        self.angle_offset = rospy.get_param("~angle_offset")
+        self.motor_type = rospy.get_param("~motor_type")
+
+
+    def auto_drive_vesc(self, steer_val, car_run_speed):
+        self.ack_msg.header.stamp = rospy.Time.now()
+        self.ack_msg.drive.steering_angle = -steer_val
+        spd = max(min(car_run_speed, self.vesc_smax), self.vesc_smin)
+        cnt = 10
+        min_v_piece = float(self.vesc_smax)/(2.0*float(cnt))
+        if ((self.last_speed > 0) and (spd <= 0)) or ((self.last_speed < 0) and (spd >= 0)):
+            #print("?", self.last_speed, spd)       
+            #self.ack_msg.drive.speed = max(min(0.43 * (self.last_speed / abs(self.last_speed)), self.vesc_smax), self.vesc_smin)
+            #self.ackerm_publisher.publish(self.ack_msg)
+            #time.sleep(self.change_vector_term)
+            reduce_v = max(abs(float(spd) - float(self.last_speed)) / float(cnt), min_v_piece)
+            vector = -(self.last_speed / abs(self.last_speed))
+            for c in range(cnt):
+                s = float(self.last_speed) + (vector * c * reduce_v)
+                if vector > 0:
+                    self.ack_msg.drive.speed = min(s, float(spd))
+                elif vector < 0:
+                    self.ack_msg.drive.speed = max(s, float(spd))
+                self.ackerm_publisher.publish(self.ack_msg)
+                time.sleep(self.change_vector_term)
+            time.sleep(self.change_vector_term)
+        if self.last_speed == 0:
+            if float(spd) < 0:
+                reduce_v = min(float(spd) / float(cnt), min_v_piece)
+            elif float(spd) > 0:
+                reduce_v = max(float(spd) / float(cnt), min_v_piece)
+            else:
+                cnt = 0
+            for c in range(cnt):
+                s = float(self.last_speed) + (reduce_v * c)
+                if float(spd) < 0:
+                    self.ack_msg.drive.speed = max(s, float(spd))
+                elif float(spd) > 0:
+                    self.ack_msg.drive.speed = min(s, float(spd))
+                #print("hs", self.ack_msg.drive.speed)
+                self.ackerm_publisher.publish(self.ack_msg)
+                time.sleep(self.change_vector_term)
+            time.sleep(self.change_vector_term)
+
+        self.ack_msg.drive.speed = float(spd)
+        #print("ra, rs", -steer_val, spd)
+        self.ackerm_publisher.publish(self.ack_msg)
+        self.last_speed = float(spd)
+    
+
+    def teb_ackermann_callback(self, msg):
+        speed = msg.linear.x
+        steering_angle = - msg.angular.z
+        stop_cnt = 0
+        # self.stop = rospy.wait_for_message("is_stop", IsStop, timeout=3)
+        # # wait_for_message -> subscribe
+                
+        if self.goal_manager is not None:
+            if self.goal_manager.stop_line and stop_cnt == 0:
+                speed = 0
+                self.auto_drive(steering_angle, speed)
+                rospy.sleep(3.)
+                stop_cnt += 1
+            
+            elif self.goal_manager.stop_line and stop_cnt != 0:
+                self.auto_drive(steering_angle, speed)
+                stop_cnt = 0
+
+            else:
+                self.auto_drive(steering_angle, speed)
+        
+        else:
+            self.auto_drive(steering_angle, speed)
+
+
+if __name__ == '__main__':
+    m = motor(sys.argv[1])
