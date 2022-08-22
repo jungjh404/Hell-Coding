dd67f558d3c8fc911db82558eec9fbec4b55a4be
jungjh404 <jungjh404@gmail.com>
2022-08-22 21:36:20 +0900

    lane_scan v1

--- main_ros/scripts/lane_to_scan.py
+++ main_ros/scripts/lane_to_scan.py
@@ -4,7 +4,6 @@
 import numpy as np
 import cv2, math
 import rospy
-import pandas as pd
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 #from xycar_msgs.msg import xycar_motor
@@ -14,14 +13,13 @@ from sensor_msgs.msg import LaserScan
 
 
 # from hell_coding.msg import _IsStop
-
 class Publishint():
     def initfunc(self, llx , rlx, lly, rly):
         #rospy.init_node('laser_scan_publisher')
 
-        scan_pub = rospy.Publisher('lane_scan', LaserScan, queue_size=1)
+        scan_pub = rospy.Publisher('lane_scan', LaserScan, queue_size=50)
     
-        num_readings = 5000 #1000 does not warp angle
+        num_readings = 2160 #1000 does not warp angle
         laser_frequency = 40
 
         count = 0
@@ -45,7 +43,7 @@ class Publishint():
             if (llx[i] > 0):
                 theta_l.append(math.atan(lly[i]/llx[i]))
             else:
-                theta_l.append(math.atan(lly[i]/llx[i]) + math.pi)
+                theta_l.append(math.atan(lly[i]/llx[i]) + 3.14)
             dist_l.append(math.sqrt( math.pow(llx[i],2)+math.pow(lly[i],2) ))
         
         for i in range(0, len(rlx)):
@@ -53,118 +51,67 @@ class Publishint():
             if (rlx[i] > 0):
                 theta_r.append(math.atan(rly[i]/rlx[i]))
             else:
-                theta_r.append(math.atan(rly[i]/rlx[i]) + math.pi)
+                theta_r.append(math.atan(rly[i]/rlx[i]) + 3.14)
             dist_r.append(math.sqrt( math.pow(rlx[i],2)+math.pow(rly[i],2) ))
             # sort is need because lidar spins continously
             # if condition is not met, laserscan is not shown
         
         #theta_l.sort()
+
+        #####################################################################################
+        dict = {name:value for name, value in zip(theta_l, dist_l)}
+        dict_r = {name:value for name, value in zip(theta_r, dist_r)}
+        #print(dict)
+        #theta_l = np.array(theta_l)
+
+        l1=0
+        l2=0
+        l3=0
+        r1=0
+        r2=0
+        r3=0
+
+        if len(theta_l) != 0:
+            l1 = float(np.percentile(theta_l , 25, interpolation='linear'))
+            l2 = float(np.percentile(theta_l , 50, interpolation='linear'))
+            l3 = float(np.percentile(theta_l , 75, interpolation='linear'))
+        if len(theta_r) != 0:
+            r1 = float(np.percentile(theta_r , 25, interpolation='linear'))
+            r2 = float(np.percentile(theta_r , 50, interpolation='linear'))
+            r3 = float(np.percentile(theta_r , 75, interpolation='linear'))
+
+        s1 = sorted({key:value for key, value in dict.items() if key < l1}.items()) # if key < l1
+        s2 = sorted({key:value for key, value in dict.items() if key >= l1 and key < l2}.items())
+        s3 = sorted({key:value for key, value in dict.items() if key >= l2 and key < l3}.items())
+        s4 = sorted({key:value for key, value in dict.items() if key >= l3}.items())
         
-        dict = {'degree': theta_l, 'distance': dist_l}
-        df = pd.DataFrame(dict)
+        p1 = sorted({key:value for key, value in dict_r.items() if key < r1}.items())
+        p2 = sorted({key:value for key, value in dict_r.items() if key >= r1 and key < r2}.items())
+        p3 = sorted({key:value for key, value in dict_r.items() if key >= r2 and key < r3}.items())
+        p4 = sorted({key:value for key, value in dict_r.items() if key >= r3}.items())
+
+        #print(p1)
+
+        smp1 = s1[::cnt(s1)]
+        smp2 = s2[::cnt(s2)]
+        smp3 = s3[::cnt(s3)]
+        smp4 = s4[::cnt(s4)]
         
-        q_1 = df.degree.quantile(0.25)
-        q_2 = df.degree.quantile(0.5)
-        q_3 = df.degree.quantile(0.75)
+        smp_fin = smp1 + smp2 + smp3 + smp4
+        print(len(smp1))
+        print(len(smp_fin))
+        rmp1 = p1[::cnt(p1)]
+        rmp2 = p2[::cnt(p2)]
+        rmp3 = p3[::cnt(p3)]
+        rmp4 = p4[::cnt(p4)]
 
-        #subset by percentile
-        sub1 = df[ df.loc[:,'degree'] < q_1].sort_values(by = 'degree', inplace = False)
-        sub2 = df[ (df.loc[:,'degree'] >= q_1) & (df.loc[:,'degree'] < q_2) ].sort_values(by = 'degree', inplace = False)
-        sub3 = df[ (df.loc[:,'degree'] >= q_2) & (df.loc[:,'degree'] < q_3) ].sort_values(by = 'degree', inplace = False)
-        sub4 = df[ df.loc[:,'degree'] >= q_3].sort_values(by = 'degree', inplace = False)
+        rmp_fin = rmp1 + rmp2+ rmp3 + rmp4
 
-        sub1.reset_index(drop = True)
-        sub2.reset_index(drop = True)
-        sub3.reset_index(drop = True)
-        sub4.reset_index(drop = True)
-
-        rate1 = int(sub1['degree'].count() / 50)
-        rate2 = int(sub2['degree'].count() / 50)
-        rate3 = int(sub3['degree'].count() / 50)
-        rate4 = int(sub4['degree'].count() / 50)
-
-        #prevent error when rate is 0
-        if rate1 == 0:
-            rate1 = 1
-        if rate2 == 0:
-            rate2 = 1
-        if rate3 == 0:
-            rate3 = 1
-        if rate4 == 0:
-            rate4 = 1
-
-        #sample of about 50 points
-        samp1 = sub1[::rate1].reset_index(drop = True)
-        samp2 = sub2[::rate2].reset_index(drop = True)
-        samp3 = sub3[::rate3].reset_index(drop = True)
-        samp4 = sub4[::rate4].reset_index(drop = True)
-
-        #can also concat right lanes to 'frames'
-        #integrate into one dataframe
-        frames = [samp1,samp2,samp3,samp4]
-        samp_final_l = pd.concat(frames)
-        #print(samp1.iloc[1][0])
-        #print(samp_final.head())
-        #print(samp_final.describe())
-        #################################################################################
-
-        dict_r = {'degree': theta_r, 'distance': dist_r}
-        df_r = pd.DataFrame(dict_r)
         
-        q_1r = df_r.degree.quantile(0.25)
-        q_2r = df_r.degree.quantile(0.5)
-        q_3r = df_r.degree.quantile(0.75)
-
-        #subset by percentile
-        sub1r = df_r[ df_r.loc[:,'degree'] < q_1r].sort_values(by = 'degree', inplace = False)
-        sub2r = df_r[ (df_r.loc[:,'degree'] >= q_1r) & (df_r.loc[:,'degree'] < q_2r) ].sort_values(by = 'degree', inplace = False)
-        sub3r = df_r[ (df_r.loc[:,'degree'] >= q_2r) & (df_r.loc[:,'degree'] < q_3r) ].sort_values(by = 'degree', inplace = False)
-        sub4r = df_r[ df_r.loc[:,'degree'] >= q_3r].sort_values(by = 'degree', inplace = False)
-
-        #df_r = df_r.sort_values(by = 'degree', inplace = False)
-        #sub1r = df_r[ df_r.loc[:,'degree'] < q_1r]
-        #sub2r = df_r[ (df_r.loc[:,'degree'] >= q_1r) & (df_r.loc[:,'degree'] < q_2r) ]
-        #sub3r = df_r[ (df_r.loc[:,'degree'] >= q_2r) & (df_r.loc[:,'degree'] < q_3r) ]
-        #sub4r = df_r[ df_r.loc[:,'degree'] >= q_3r]
+        ###############################################################################
         
-        sub1r.reset_index(drop = True)
-        sub2r.reset_index(drop = True)
-        sub3r.reset_index(drop = True)
-        sub4r.reset_index(drop = True)
-
-        rate1_r = int(sub1r['degree'].count() / 50)
-        rate2_r = int(sub2r['degree'].count() / 50)
-        rate3_r = int(sub3r['degree'].count() / 50)
-        rate4_r = int(sub4r['degree'].count() / 50)
-
-        #prevent error when rate is 0
-        if rate1_r == 0:
-            rate1_r = 1
-        if rate2_r == 0:
-            rate2_r = 1
-        if rate3_r == 0:
-            rate3_r = 1
-        if rate4_r == 0:
-            rate4_r = 1
-
-        #sample of about 50 points
-        samp1_r = sub1r[::rate1_r].reset_index(drop = True)
-        samp2_r = sub2r[::rate2_r].reset_index(drop = True)
-        samp3_r = sub3r[::rate3_r].reset_index(drop = True)
-        samp4_r = sub4r[::rate4_r].reset_index(drop = True)
-
-        #can also concat right lanes to 'frames' 
-        frames_r = [samp1_r,samp2_r,samp3_r,samp4_r]
-
-        #print(frames_r)
-        #print('---------------------')
-        samp_final_r = pd.concat(frames_r)
-        #samp_final_r = samp_final_r.sort_values(by='degree', inplace = False)
-        #samp_final_r.reset_index(drop = True)
-
-        #################################################################################
         current_time = rospy.Time.now()
-    
+        
         scan = LaserScan()
 
             # range is in [m]
@@ -176,44 +123,52 @@ class Publishint():
         scan.time_increment = (1.0 / laser_frequency) / (num_readings)
         scan.range_min = 0.0
         scan.range_max = 100.0
-        scan.ranges = [0] * 5000
-        scan.intensities = [0] * 5000
+    
+        scan.ranges = [0]*num_readings
+        scan.intensities = [0]*num_readings
 
-        # r, theta = np.meshgrid(dist_r, theta_r)
-        templ=samp_final_l['degree'].values.tolist()
-        tempr=samp_final_r['degree'].values.tolist()
-        distl=samp_final_l['distance'].values.tolist()
-        distr=samp_final_r['distance'].values.tolist()
+
+        
 
         j = 0
         k = 0
+        
         for i in range(0, num_readings):
                 #if (j == len(x)):
                 #    break
 
                 #if theta is within the increments
-            
-            if (j < samp_final_l['degree'].count() and abs(i * scan.angle_increment -templ[j]) < error ):
+            if (j < len(smp_fin) and abs(i * scan.angle_increment -smp_fin[j][0]) < error ):
                 try:
-                    idx = int(templ[j] / math.pi * num_readings)
-                    scan.ranges[idx] = distl[j]
+                    idx = int(smp_fin[j][0] / math.pi * num_readings)
+                    scan.ranges[idx] = smp_fin[j][1]
+                    print(smp_fin[j][1])
                     scan.intensities[idx] = 1
                     j+=1
                 except IndexError:
                     pass
                 #if use pass, there is time delay, resulting in error
-
+        
         for i in range(0, num_readings):
-            if (k < samp_final_r['degree'].count() and abs(i * scan.angle_increment - tempr[k]< error )): #samp_final_r.iloc[k][0])
+            if (k < len(rmp_fin) and abs(i * scan.angle_increment - rmp_fin[k][0]< error )):
                 try:
-                    idx = int(tempr[k] / math.pi * num_readings)
-                    scan.ranges[idx] = distr[k]
+                    idx = int(rmp_fin[k][0] / math.pi * num_readings)
+                    scan.ranges[idx] = rmp_fin[k][1]
+                    print(rmp_fin[k][1])
                     scan.intensities[idx] = 1
                     k+=1
                 except IndexError:
-                    pass
+                    pass    
+        
         scan_pub.publish(scan)
 
+def cnt(arr):
+    r = 0
+    if (len(arr)/50) == 0:
+        r = 1
+    else:
+        r = len(arr)/50
+    return r
 
 
 
